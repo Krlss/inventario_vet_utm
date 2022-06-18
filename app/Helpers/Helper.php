9c9ef11727f9bbf416f4147b44c373d801c94c5c
Pedro Alcivar <palcivar9694@utm.edu.ec>
2022-06-18 11:47:50 -0500

    flashmessage in helper

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -5,3 +5,13 @@ function generateProfilePhotoPath($string)
     return 'https://ui-avatars.com/api/?name=' . $string[0] . '&color=fff&background=FFB509&bold=true&length=1';
 }
 
+function flashMessage($message, $type = 'success')
+{
+    if ($type == 'success') {
+        $type = 'primary';
+    } elseif ($type == 'error') {
+        $type = 'danger';
+    }
+
+    return '<div class="alert alert-' . $type . ' alert-dismissible fade show" role="alert">' . $message . '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+}
